2fa910a78e7c034b3624c66f11bd4aec6fae8d58
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-10 19:04:21 +0300

    About page

---
+++ src/components/About.js
@@ -0,0 +1,101 @@
+import React from "react";
+function About() {
+  return (
+    <div style={{ backgroundColor: "#ffe4c4", color: "black" }}>
+      <div className=" container position-relative  justify-content-around align-items-center ">
+        <h1 style={{ textAlign: "center" }}>What Is Write Out Loud?</h1>
+        <p>
+          Write Out Loud is a national (indeed, international) hub for
+          participation in poetry, encouraging everyone who writes poetry – from
+          still-too-nervous-to-do-open-mic to Nobel Prize winner – to share
+          their words with others. We're not just the website (more on that
+          later), but seeing as you're here, let's tell you all about that
+          first. <br />
+          <br />
+          The Write Out Loud website has been around since 2005. It's unique in
+          what it brings together, and what it offers to our members. Here
+          you’ll find our unparallelled news section (covering news from
+          publishers, poets, events, gig reviews and book reviews), our gig
+          guide (where you can find out what's happening near you) and, of
+          course, our blogs (where you can post your work, and read and comment
+          on work posted by others).
+        </p>
+
+        <p>
+          All of this is designed to encourage interaction in a comfortable and
+          inclusive atmosphere, which helps explain why over 45,000 people visit
+          the website each month, and why they keep coming back. In order for
+          you to get the most benefit out of the Write Out Loud site, here’s a
+          guide to what you’ll find here.
+        </p>
+        <ul>
+          <li>
+            <h3>News</h3>
+            <p>
+              The latest news from the world of poetry, including reviews of
+              events and books, details of competitions, and much much more. If
+              you think there’s something we should know about, or you want to
+              submit a review of an event you have attended, you can contact our
+              Features editor at news@writeoutloud.net
+            </p>
+          </li>
+          <li>
+            <h3>Gig guide</h3>
+            <p>
+              Poetry events are an opportunity to share your work, meet up with
+              other poets, socialise, and listen to a wide range of styles,
+              ideas, forms, voices, and experiences. Our gig guide is the best
+              way to find out what's going on in your area, and is one of the
+              most comprehensive guides there is. If you find a night near you
+              which isn't using it, do let them know they can post their events
+              on the site for free (and updating or changing it is easy). If
+              they need a helping hand, they can email gig@writeoutloud.net
+            </p>
+          </li>
+          <li>
+            <h3>Blogs</h3>
+            <p>
+              The place where you can share your work and read work other WOL
+              members have posted. Unsurprisingly, this is one of the busiest
+              areas of the site! If someone's poem moves you, makes you laugh,
+              or inspires you to go off and write a piece of your own, do take a
+              minute or two to let the author know by commenting on their work.
+              We all love to get feedback on what we've posted.
+            </p>
+          </li>
+          <li>
+            <h3>Profiles</h3>
+            <p>
+              Creating a profile when you join Write Out Loud gives you the
+              opportunity to tell us a bit about yourself. What brought you
+              here? What does poetry do for you? What do you hope others will
+              get from your work? Let us all know.
+            </p>
+          </li>
+          <li>
+            <h3>Galleries</h3>
+            <p>
+              Photos from poetry events and readings, showing poets of all ages,
+              backgrounds, poetic styles and levels of experience.
+            </p>
+          </li>
+        </ul>
+        <p>
+          As we said earlier, WOL isn’t just the website. In 2015, we became a
+          Community Interest Company (CIC) with a mission to make poetry more
+          widely appreciated and available. We believe passionately in the power
+          of poetry to bring enjoyment, fulfillment and positive change to
+          people’s lives, and our aim is to bring more people to poetry and
+          poetry to more people. WOL is an information resource for people
+          wanting to set up poetry events where they live, and we also support
+          individual poets, poetry organisations and groups across the country,
+          via a monthly newsletter (and this website). As you’ll see from the
+          gig guide, we have monthly open-floor poetry nights in Marsden,
+          Huddersfield, Middleton, Sale, Stockport and Wigan, as well as
+          irregular events across the North of England.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default About;
